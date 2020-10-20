4201cf54abf4c7ab5edb8a26c4fbc0fd04561ff3
Andrei Korolev <andreikoroleffs@gmail.com>
2020-10-20 16:48:38 +0300

    Создан компонент AudioPlayer. Audio Player подключен в GenreQuestionScreen и ArtistQuestionScreen

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -1,82 +1,95 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import {GameType} from "../../const";
+import AudioPlayer from "../audio-player/audio-player";
 
-const ArtistQuestionScreen = (props) => {
-  const {onAnswer, question} = props;
-  const {
-    answers,
-    song,
-  } = question;
+class ArtistQuestionScreen extends PureComponent {
+  constructor(props) {
+    super(props);
 
-  return (
-    <main className="app">
-      <svg xmlns="http://www.w3.org/2000/svg" style={{position: `absolute`, left: `-1200em`}}>
-        <filter id="blur">
-          <feGaussianBlur in="SourceGraphic" stdDeviation="5"></feGaussianBlur>
-          <feOffset dx="0" dy="0"></feOffset>
-          <feMerge>
-            <feMergeNode></feMergeNode>
-            <feMergeNode in="SourceGraphic"></feMergeNode>
-          </feMerge>
-        </filter>
-      </svg>
-      <section className="main" id="root">
-        <section className="game game--artist">
-          <header className="game__header">
-            <a className="game__back" href="#">
-              <span className="visually-hidden">Сыграть ещё раз</span>
-              <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
-            </a>
+    this.state = {
+      isPlaying: true,
+    };
+  }
 
-            <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
-              <circle className="timer__line" cx="390" cy="390" r="370" style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}} />
-            </svg>
+  render() {
+    const {isPlaying} = this.state;
+    const {onAnswer, question} = this.props;
+    const {
+      answers,
+      song,
+    } = question;
 
-            <div className="game__mistakes">
-              <div className="wrong"></div>
-              <div className="wrong"></div>
-              <div className="wrong"></div>
-            </div>
-          </header>
+    return (
+      <main className="app">
+        <svg xmlns="http://www.w3.org/2000/svg" style={{position: `absolute`, left: `-1200em`}}>
+          <filter id="blur">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="5"></feGaussianBlur>
+            <feOffset dx="0" dy="0"></feOffset>
+            <feMerge>
+              <feMergeNode></feMergeNode>
+              <feMergeNode in="SourceGraphic"></feMergeNode>
+            </feMerge>
+          </filter>
+        </svg>
+        <section className="main" id="root">
+          <section className="game game--artist">
+            <header className="game__header">
+              <a className="game__back" href="#">
+                <span className="visually-hidden">Сыграть ещё раз</span>
+                <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
+              </a>
 
-          <section className="game__screen">
-            <h2 className="game__title">Кто исполняет эту песню?</h2>
-            <div className="game__track">
-              <div className="track">
-                <button className="track__button track__button--play" type="button"></button>
-                <div className="track__status">
-                  <audio
+              <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+                <circle className="timer__line" cx="390" cy="390" r="370" style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}} />
+              </svg>
+
+              <div className="game__mistakes">
+                <div className="wrong"></div>
+                <div className="wrong"></div>
+                <div className="wrong"></div>
+              </div>
+            </header>
+
+            <section className="game__screen">
+              <h2 className="game__title">Кто исполняет эту песню?</h2>
+              <div className="game__track">
+                <div className="track">
+                  <AudioPlayer
+                    onPlayButtonClick={() => {
+                      this.setState({isPlaying: !isPlaying});
+                    }}
+                    isPlaying={isPlaying}
                     src={song.src}
                   />
                 </div>
               </div>
-            </div>
 
-            <form className="game__artist">
-              {answers.map((answer, i) => (
-                <div key={answer.artist} className="artist">
-                  <input className="artist__input visually-hidden" type="radio" name="answer"
-                    id={`answer-${i}`}
-                    value={`answer-${i}`}
-                    onChange={(evt) => {
-                      evt.preventDefault();
-                      onAnswer(question, answer);
-                    }}
-                  />
-                  <label className="artist__name" htmlFor={`answer-${i}`}>
-                    <img className="artist__picture" src={answer.picture} alt="Пелагея" />
-                    {answer.artist}
-                  </label>
-                </div>
-              ))}
-            </form>
+              <form className="game__artist">
+                {answers.map((answer, i) => (
+                  <div key={answer.artist} className="artist">
+                    <input className="artist__input visually-hidden" type="radio" name="answer"
+                      id={`answer-${i}`}
+                      value={`answer-${i}`}
+                      onChange={(evt) => {
+                        evt.preventDefault();
+                        onAnswer(question, answer);
+                      }}
+                    />
+                    <label className="artist__name" htmlFor={`answer-${i}`}>
+                      <img className="artist__picture" src={answer.picture} alt="Пелагея" />
+                      {answer.artist}
+                    </label>
+                  </div>
+                ))}
+              </form>
+            </section>
           </section>
         </section>
-      </section>
-    </main>
-  );
-};
+      </main>
+    );
+  }
+}
 
 ArtistQuestionScreen.propTypes = {
   onAnswer: PropTypes.func.isRequired,
--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -1,6 +1,7 @@
 import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import {GameType} from "../../const";
+import AudioPlayer from "../audio-player/audio-player";
 
 
 class GengeQuestionScreen extends PureComponent {
@@ -8,13 +9,14 @@ class GengeQuestionScreen extends PureComponent {
     super(props);
 
     this.state = {
+      activePlayer: 0,
       answers: [false, false, false, false],
     };
   }
 
   render() {
     const {onAnswer, question} = this.props;
-    const {answers: userAnswers} = this.state;
+    const {answers: userAnswers, activePlayer} = this.state;
     const {
       answers,
       genre,
@@ -61,12 +63,15 @@ class GengeQuestionScreen extends PureComponent {
               >
                 {answers.map((answer, i) => (
                   <div key={`${i}-${answer.src}`} className="track">
-                    <button className="track__button track__button--play" type="button"></button>
-                    <div className="track__status">
-                      <audio
-                        src={answer.src}
-                      />
-                    </div>
+                    <AudioPlayer
+                      onPlayButtonClick={() => {
+                        this.setState({
+                          activePlayer: activePlayer === i ? -1 : i,
+                        });
+                      }}
+                      isPlaying={i === activePlayer}
+                      src={answer.src}
+                    />
                     <div className="game__answer">
                       <input className="game__input visually-hidden" type="checkbox" name="answer"
                         value={`anwer-${i}`}
